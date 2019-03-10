89ac17a78be0fde2b169111e2007cb21ed50fa08
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-03-10 18:13:16 -0500

    Update PaintColor.java

--- PaintColor.java
+++ PaintColor.java
@@ -2,18 +2,20 @@ package carcraft;
 
 public class PaintColor extends CarOptions() {
 	
+	protected CarOptions car;
 	public String color;
 	
-	public PaintColor(String color)
+	public PaintColor(CarOptions car, String color)
 	{
 		System.out.println("Paint Color constructor");
+		this.car = car;
 		this.color = color;
 	}
 	
 	@Override
 	public String getDescription()
 	{
-		return super.getDescription() + " painted " + this.color;
+		return " painted " + this.color;
 	}
 	
 }
