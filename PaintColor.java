f6259342c785c15e06d7f8129015c0f3aae4b260
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-03-10 17:48:13 -0500

    Update PaintColor.java

--- PaintColor.java
+++ PaintColor.java
@@ -1,25 +1,19 @@
-
-import java.awt.Color;
-
 package carcraft;
 
 public class PaintColor extends CarOptions() {
 	
-	public Color color;
+	public String color;
 	
-	public AirScoop(Color color)
+	public AirScoop(String color)
 	{
 		System.out.println("Paint Color constructor");
-		
 		this.color = color;
 	}
 	
 	@Override
 	public String getDescription()
 	{
-		return super.getDescription() + "with r" + color.getRed() 
-		+ "g" + color.getGreen() 
-		+ "b" + color.getBlue() + " paint color.";
+		return super.getDescription() + " painted " + this.color;
 	}
 	
 }
