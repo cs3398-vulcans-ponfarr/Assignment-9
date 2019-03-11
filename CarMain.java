39a5dcb201b9c2b2f10a18a439f489aaa59035b3
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-03-11 11:23:12 -0500

    Update CarMain.java

--- CarMain.java
+++ CarMain.java
@@ -7,12 +7,12 @@ public class CarMain
 	CarOptions car2 = new SuperCharger(new PaintColor(new SportWheels(new AirScoop(new CarOptions(), "aluminum"), "17-inch"), "red"), "centrifugal");
 	CarOptions car3 = new AirScoop(new PaintColor(new SportWheels(new SuperCharger(new CarOptions(), "axial compressor"), "18-inch"), "black"), "classic");
 	CarOptions car4 = new PaintColor(new SportWheels(new CarOptions(), "40-inch"), "hot-pink");
-	CarOptions car5 = new SuperCharger(new PaintColor(new SportWheels(new CarOptions(), "17-inch"), "matte black", "pressure wave");
+	CarOptions car5 = new SuperCharger(new PaintColor(new SportWheels(new CarOptions(), "17-inch"), "matte black"), "pressure wave");
 	CarOptions car6 = new SportWheels(new PaintColor(new CarOptions(), "magenta"), "15-inch");
-	car1.getDescription();
-	car2.getDescription();
-	car3.getDescription();
-	car4.getDescription();
-	car5.getDescription();
-	car6.getDescroption();
+	System.out.println(car1.getDescription());
+	System.out.println(car2.getDescription());
+	System.out.println(car3.getDescription());
+	System.out.println(car4.getDescription());
+	System.out.println(car5.getDescription());
+	System.out.println(car6.getDescription());
 }
