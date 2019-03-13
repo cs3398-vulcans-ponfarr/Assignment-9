792cc8a169eaf76dc474c0524742189af57a1520
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-03-13 11:50:23 -0500

    Update BasicCar.java

--- BasicCar.java
+++ BasicCar.java
@@ -4,11 +4,11 @@ package carcraft;
 public class BasicCar implements ICar {
 
     public BasicCar() {
-		System.out.print(" BasicCar Constructor\n");
+		//System.out.print(" BasicCar Constructor\n");
 	}
 
     @Override
-    public String GetDescription() {
+    public String getDescription() {
         return "A Basic Car";
     }
 }
