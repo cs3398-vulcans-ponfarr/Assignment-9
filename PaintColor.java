d03c98881241d621cea285fec7647b25c72ce259
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-03-13 09:45:57 -0500

    Update PaintColor.java

--- PaintColor.java
+++ PaintColor.java
@@ -15,7 +15,7 @@ public class PaintColor extends CarOptions {
 	@Override
 	public String getDescription()
 	{
-		return super.getDescription() + " painted " + this.color;
+		return car.getDescription() + " painted " + this.color;
 	}
 	
 }
